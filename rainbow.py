4857dabc9d6a8b198d004c93c4d53edd4b9a969b
Mihir Pathak <mihirpathak97@gmail.com>
2018-06-24 14:52:02 +0530

    clean up

--- rainbow.py
+++ rainbow.py
@@ -6,7 +6,7 @@ from lib import metadata
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Embed metadata into audio files using spotify API")
-    parser.add_argument('-f', '--file', help='Absolute file name')
+    parser.add_argument('-f', '--file', help='Absolute path of audio file')
     parser.add_argument('-id', help='Spotify ID of song')
     args = parser.parse_args()
     metadata.generate_and_embed_metadata(args.file, args.id)
