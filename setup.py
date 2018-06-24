45d8530cbff56b25d5ff50670e542789f45b834b
Mihir Pathak <mihirpathak97@gmail.com>
2018-06-24 09:47:50 +0530

    freeze: set custom build directory

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from cx_Freeze import setup, Executable
 
 packages = ["queue", "idna"]  # need to explicitly include these modules :(
 
-build_exe_options = {"packages": packages, "excludes": ["tkinter"]}
+build_exe_options = {"packages": packages, "excludes": ["tkinter"], "build_exe": "build"}
 
 setup(  
   name = "rainbow",
